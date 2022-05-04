7e0ecca3af242add680c0b3940e2c605e2b41906
ivan <ivan62196211@gmail.com>
2022-05-04 15:13:51 +0300

    laravel_start

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import App from './components/App.vue'
+import Router from './router/index';
+
+createApp(App).use(Router).mount("#app")
---
+++ resources/js/router/routers/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hiiii</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+</style>
